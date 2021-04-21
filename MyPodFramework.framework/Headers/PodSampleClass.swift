290fed355453b43bd50cb2e65595a7315d2c4cd0
Neha <narnh@allstate.com>
2021-04-22 00:23:40 +0530

    updated framework

---
+++ MyPodFramework.framework/Headers/PodSampleClass.swift
@@ -0,0 +1,14 @@
+//
+//  PodSampleClass.swift
+//  MyPodFramework
+//
+//  Created by Arneja, Neha on 21/04/21.
+//
+
+import Foundation
+
+public class PodSampleClass {
+    public class func initialiseFramework() {
+        print("Framework initiated")
+    }
+}
