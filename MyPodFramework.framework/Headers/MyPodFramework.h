34e90f25a07846deeb4bba0a87900d8bdbd95947
Neha <narnh@allstate.com>
2021-04-22 00:14:07 +0530

    added framework

---
+++ MyPodFramework.framework/Headers/MyPodFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyPodFramework.h
+//  MyPodFramework
+//
+//  Created by Arneja, Neha on 21/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyPodFramework.
+FOUNDATION_EXPORT double MyPodFrameworkVersionNumber;
+
+//! Project version string for MyPodFramework.
+FOUNDATION_EXPORT const unsigned char MyPodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPodFramework/PublicHeader.h>
+
+
